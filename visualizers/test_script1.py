73fcad5da51185ce43efe2dfc1fe320762c152c4
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-13 14:46:24 -0500

    covers window just has menu bar

--- visualizers/test_script1.py
+++ visualizers/test_script1.py
@@ -1,3 +1,4 @@
+import matplotlib
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,9 +21,23 @@ stream = p.open(format=FORMAT,
                 frames_per_buffer=CHUNK)
 
 # Create plot
+matplotlib.rcParams["figure.figsize"] = (15,12)
 fig, ax = plt.subplots()
+#
+fig.patch.set_visible(False)  # Hide figure background
+ax.set_xticks([])  # Hide x-axis ticks
+ax.set_yticks([])  # Hide y-axis ticks
+
 x = np.arange(0, 2 * CHUNK, 2)
 line, = ax.plot(x, np.random.rand(CHUNK))
+
+# Maximize figure window and position it to the top-left corner
+plt.get_current_fig_manager().window.state('zoomed')  # Maximize window
+plt.get_current_fig_manager().window.geometry("+0+0")  # Move window to top-left corner
+
+# Remove margins at the top and left
+plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
 
 # Update function
 def update_plot(frame):
@@ -33,6 +48,7 @@ def update_plot(frame):
 # Start animation
 ani = FuncAnimation(fig, update_plot, blit=True)
 plt.show()
+
 
 # Close the stream and terminate PyAudio
 stream.stop_stream()
